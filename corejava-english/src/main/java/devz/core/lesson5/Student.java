f662ecb8325f21acf4b1a76b0ea40e7da75a7b45
Ravinath Fernando <devzonedo@gmail.com>
2021-11-18 20:32:45 +0530

    lesson-5

---
+++ corejava-english/src/main/java/devz/core/lesson5/Student.java
@@ -0,0 +1,20 @@
+
+package devz.core.lesson5;
+
+
+public class Student {
+    
+   int id;
+   String fname;
+   String lname;
+   String nic;
+   int tp;
+   String address_line1;
+   String address_line2;
+   String address_line3;
+   String address_line4;
+   String city;
+   
+   
+   
+}
---
+++ corejava-sinhala/src/main/java/devz/core/lesson5/Student.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devz.core.lesson5;
+
+/**
+ *
+ * @author devzo
+ */
+public class Student {
+    
+    int age;
+    String fname;
+    String lname;
+    
+}
